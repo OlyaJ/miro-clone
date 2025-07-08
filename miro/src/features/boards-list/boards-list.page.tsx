bea15be2229ec82bdec90002617664060f0ef88c
OlyaJ <schastnayaolga92@gmail.com>
2025-07-08 22:12:42 +0300

    fixes - router

--- miro/src/features/boards-list/boards-list.page.tsx
+++ miro/src/features/boards-list/boards-list.page.tsx
@@ -1,7 +1,11 @@
+import { ROUTES } from "@/shared/model/routes";
+import { Link, href } from "react-router-dom";
+
 function BoardsListPage() {
   return (
     <div>
-      <h1>Boards list</h1>
+      <h1>Boards list</h1> 
+      <Link to={href(ROUTES.BOARD, {boardId: "1"})}>Board 1</Link>
     </div>
   );
 }
