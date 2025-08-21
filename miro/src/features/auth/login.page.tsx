f595e0cb0f8b084a346eeb472fe4a86059ef9ca2
OlyaJ <schastnayaolga92@gmail.com>
2025-08-21 09:50:39 +0300

    login page

--- miro/src/features/auth/login.page.tsx
+++ miro/src/features/auth/login.page.tsx
@@ -1,30 +1,21 @@
-import { rqClient } from "@/shared/api/instance";
 import { ROUTES } from "@/shared/model/routes";
-import { Button } from "@/shared/ui/kit/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/shared/ui/kit/card";
 import { Link } from "react-router-dom";
+import { AuthLayout } from "./auth-layout";
 
 function LoginPage() {
-
-  const loginMutation = rqClient.useMutation("post", "/auth/login");
-  return <main className="grow flex flex-cal pt-[200px] items-center">
-    <Card className="w-full max-w-[400px]">
-      <CardHeader>
-        <CardTitle>Вход в систему</CardTitle>
-        <CardDescription>
-          Введите ваш email и пароль для входа в систему
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-
-      </CardContent>
-      <CardFooter>
-          <p>
-            Нет аккаунта? <Button asChild variant="link"><Link to={ROUTES.REGISTER}>Зарегистрироваться</Link></Button>
-          </p>
-      </CardFooter>
-    </Card>
-  </main>
+  return (
+    <AuthLayout
+        title="Вход в систему"
+        description="Введите ваш email и пароль для входа в систему"
+        form={<form></form>}
+        footerText={
+          <>
+            Нет аккаунта? <Link to={ROUTES.REGISTER}>Зарегистрироваться</Link>
+          </>
+        }
+    >
+    </AuthLayout>
+  )
 }
 
 export const Component = LoginPage;
--- miro/src/features/auth/register.page.tsx
+++ miro/src/features/auth/register.page.tsx
@@ -1,5 +1,21 @@
+import { ROUTES } from "@/shared/model/routes";
+import { AuthLayout } from "./auth-layout";
+import { Link } from "react-router-dom";
+
 function RegisterPage() {
-  return <div>Register page</div>;
+  return (
+    <AuthLayout
+        title="Регистрация"
+        description="Введите ваш email и пароль для входа в систему"
+        form={<form></form>}
+        footerText={
+          <>
+            Нет аккаунта? <Link to={ROUTES.LOGIN}>Войти</Link>
+          </>
+        }
+    >
+    </AuthLayout>
+  )
 }
 
 export const Component = RegisterPage;
