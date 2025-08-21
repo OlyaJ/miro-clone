d0d837a6a7dca8e41f58e7034bed4a83f1450d9e
OlyaJ <schastnayaolga92@gmail.com>
2025-08-21 10:54:09 +0300

    login page

---
+++ miro/src/features/auth/login-form.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/shared/ui/kit/button";
+import { FormField, FormItem, FormLabel, FormControl, FormMessage, Form } from "@/shared/ui/kit/form";
+import { Input } from "@/shared/ui/kit/input";
+import { useForm } from "react-hook-form";
+import {z} from "zod"; 
+import {zodResolver} from "@hookform/resolvers/zod"
+
+const loginSchema = z.object({
+    email: z.string({ required_error: "Email обязателен" })
+            .email("Неверный email"),
+    password: z.string({ required_error: "Пароль обязателен" })
+            .min(6, "Пароль должен быть не менее 6 символов"),
+})
+
+
+export function LoginForm() {
+    const form = useForm({
+        resolver: zodResolver(loginSchema),
+        defaultValues: { email: "", password: "" },  
+      })
+      
+
+const onSubmit = form.handleSubmit((data)=>{
+    console.log(data)
+})
+
+    return (
+        <Form {...form}>
+            <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+            <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                            <Input placeholder="admin@gmail.com" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                            <Input placeholder="********" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <Button type="submit">Войти</Button>
+            </form>
+        </Form>
+    )
+}
--- miro/src/features/auth/login.page.tsx
+++ miro/src/features/auth/login.page.tsx
@@ -1,13 +1,14 @@
 import { ROUTES } from "@/shared/model/routes";
 import { Link } from "react-router-dom";
 import { AuthLayout } from "./auth-layout";
+import { LoginForm } from "./login-form";
 
 function LoginPage() {
   return (
     <AuthLayout
         title="Вход в систему"
         description="Введите ваш email и пароль для входа в систему"
-        form={<form></form>}
+        form={<LoginForm/>}
         footerText={
           <>
             Нет аккаунта? <Link to={ROUTES.REGISTER}>Зарегистрироваться</Link>
